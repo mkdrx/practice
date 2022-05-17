370e85974bb76c62ea6c3a864120ca42b7396238
Markus D <markusdrx@gmail.com>
2022-05-17 21:38:27 +0200

    added useReducer for password validation, and brought back useEffect with dependencies(using object destructuring to access property)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,14 +14,29 @@ const emailReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: "",
     isValid: null,
   });
@@ -34,45 +49,40 @@ const Login = (props) => {
     };
   }, []);
 
+  // Object destructuring => we pass specific properties instead of the entire object as a dependency.
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
   // Note: - useEffect with no [] - runs on every state change
   //       - useEffect with [] but no dependencies - runs on first render, no more
   //       - useEffect with [dependencies] - runs when the component is reevaluated and the dependencies (state) changed
 
-  // clean-up function:
+  // Clean-up function:
   // - with dependencies useEffect - return () => { }; runs before after the first render
   // - with no dependencies the clean-up (return()) would run when the component is removed from the dom
 
-  // Function I want to be triggered only when array content is changed (dependencies)
-  // in this case: sets the form as valid if enteredMail has @ and enteredPassword is higher than 6
-  // its reevaluated since enteredEmail and enteredPassword are being changed with key strokes BUT
-  // added Timeout so it only validates after stop typing for 500ms and not on every single key stroke
+  // Function I want to be executed only when array content is changed (dependencies)
+  // in this case: sets the form as valid if email and password are valid
+  // input is being reevaluated every 500ms to check for validity
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
-  //   !Clean-up function before useEffect executes the above function the next time
-  //   return () => {
-  //     console.log('CLEANUP');
-  //     clearTimeout(identifier);
-  //   };
-  // }, [enteredEmail, enteredPassword]);
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+    //   Clean-up function before useEffect executes the above function the next time
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
-
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
   };
 
   const validateEmailHandler = () => {
@@ -80,12 +90,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "USER_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -107,14 +117,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
