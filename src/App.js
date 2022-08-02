db290d6fdcd2e03440ca70ee33085b900401f598
mkdrx <markusdrx@gmail.com>
2022-08-02 23:49:17 +0200

    random test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
+// Testing
 function App() {
   const ctx = useContext(AuthContext);
   return (
